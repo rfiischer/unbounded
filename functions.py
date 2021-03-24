04531849bf8c53a7903b9f92fbc9809e92b56325
Rodrigo Fischer <rfischeracc@gmail.com>
2021-03-24 20:14:37 -0300

    [try new model]

--- functions.py
+++ functions.py
@@ -44,3 +44,52 @@ def cost_function1(t, h, m1, m2):
     gradient[n + 1:] = i_e @ x.T
 
     return cost, gradient
+
+
+def cost_function2(p, h, m1, m2):
+
+    m, _ = h.shape
+    n, _ = m1.shape
+
+    ratios = p[:n].reshape((-1, 1))
+    real_d = p[n:n + m].reshape((-1, 1))
+    imag_d = p[n + m:n + 2 * m].reshape((-1, 1))
+    real_v = p[n + 2 * m:n + 2 * m + n * m]
+    imag_v = p[n + 2 * m + n * m:]
+
+    v = real_v.reshape((n, m)) + 1j * imag_v.reshape((n, m))
+    omega = ratios * m1 - m2
+    d = real_d + 1j * imag_d
+
+    h_est = v.T @ omega + d
+    err = h_est - h
+    rerr = np.real(err)
+    ierr = np.imag(err)
+    cost = 1 / 2 * np.sum(np.abs(err) ** 2)
+
+    gradient = np.zeros_like(p)
+    gradient[:n] = np.sum((rerr @ m1.T) * np.real(v.T) +
+                          (ierr @ m1.T) * np.imag(v.T), axis=0)
+
+    gradient[n:n + m] = np.sum(rerr, axis=-1)
+    gradient[n + m:n + 2 * m] = np.sum(ierr, axis=-1)
+    gradient[n + 2 * m:n + 2 * m + n * m] = (omega @ rerr.T).flatten()
+    gradient[n + 2 * m + n * m:] = (omega @ ierr.T).flatten()
+
+    return cost, gradient
+
+
+def test_grad(func, t0, r=range(10)):
+    _, grad = func(t0)
+    error = np.zeros_like(t0)
+    delta = 0.0005
+    gra = []
+    for i in r:
+        print(f"Evaluating variable {i}")
+        error[i] += delta
+        val1, _ = func(t0 + error)
+        val2, _ = func(t0 - error)
+        gra.append((val1 - val2) / (2 * delta))
+        error[i] -= delta
+
+    print(np.max(grad[:len(gra)] - gra))
---
+++ scripts/test_3.py
@@ -0,0 +1,50 @@
+"""
+Try different model, with different gamma_1 and gamma_-1
+
+It turns out not to be the right model
+"""
+
+import numpy as np
+from scipy.io import loadmat
+import matplotlib.pyplot as plt
+from scipy.optimize import fmin_tnc
+
+from functions import cost_function2
+
+
+# Load data
+data = loadmat("..\\datasets\\h_estimated.mat")
+
+# Load variables
+h_array = data['h_array']
+M = 1  # data['M'][0, 0]
+N = data['N'][0, 0]
+thetas = data['pilotMatrix4N'].astype(np.float64)
+mask1 = (thetas == 1).astype(np.float64)
+mask2 = (thetas == -1).astype(np.float64)
+
+# Assemble the vector of the samples h[3]
+test = np.arange(0, N)
+h0 = h_array[3, test].reshape((1, -1))
+
+# Perform regression
+t0 = np.concatenate(([1] * N, 0.0005 * np.random.randn(2 * (M + M * N))))
+sol, nit, rc = fmin_tnc(lambda t: cost_function2(t, h0, mask1[:, test], mask2[:, test]),
+                        t0)
+
+# Get solution
+ratios = sol[:N].reshape((-1, 1))
+d = sol[N:N + M] + 1j * sol[N + M:N + 2 * M].reshape((-1, 1))
+v = (sol[N + 2 * M:N + 2 * M + N * M] + 1j * sol[N + 2 * M + N * M:]).reshape((N, M))
+
+
+# Test in remaining data
+test_configs = ratios * mask1 - mask2
+h_est = v.T @ test_configs + d
+norm = np.sum(np.abs(h0 - np.average(h0)) ** 2)
+error = np.sum(np.abs(h_array - h_est) ** 2) / norm
+
+ex = 3
+plt.plot(np.abs(h_est).T, 'ro', label='estimated')
+plt.plot(np.abs(h_array[ex, :]), 'b*', label='true')
+plt.show()
