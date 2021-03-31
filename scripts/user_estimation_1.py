41b4ff5856d61b23bc9994660dffc4f3d749b0fc
Rodrigo Fischer <rfischeracc@gmail.com>
2021-03-31 20:32:26 -0300

    [dataset2 extraction and visualization algorithms]

---
+++ scripts/user_estimation_1.py
@@ -0,0 +1,43 @@
+from scipy.io import loadmat
+from scipy.io import savemat
+from scipy.linalg import dft
+import numpy as np
+
+
+# Load dataset
+dataset2 = loadmat("..\\datasets\\dataset2.mat")
+
+
+# Unpack constants
+K = dataset2['K'][0, 0].astype(int)
+M = dataset2['M'][0, 0].astype(int)
+N = dataset2['N'][0, 0].astype(int)
+
+
+# Unpack matrices
+pilotMatrix = dataset2['pilotMatrix']
+receivedSignal = np.transpose(dataset2['receivedSignal'], axes=[2, 0, 1])
+transmitSignal = dataset2['transmitSignal']
+del dataset2
+
+
+# Estimate channel
+F = dft(K)[:, :M]
+iF = dft(K).conj()[:M, :] / K
+h_array = iF @ receivedSignal / transmitSignal[0, 0]
+hf_array = F @ h_array
+
+savemat('../datasets/h_user.mat', {'h_user': h_array, 'M': M, 'K': K, 'N': N, 'pilotMatrix4N': pilotMatrix})
+del h_array
+
+# Estimate variance
+print("Estimating variance.")
+var = 1 / 2 * np.average(np.abs(receivedSignal - transmitSignal * hf_array) ** 2, axis=1)
+savemat('../datasets/var_user.mat', {'var_user': var, 'M': M, 'K': K, 'N': N, 'pilotMatrix4N': pilotMatrix})
+del receivedSignal
+
+# Estimate SNR
+print("Estimating SNR.")
+snr = 10 * np.log10(np.average(np.abs(transmitSignal * hf_array) ** 2, axis=1) / (2 * var))
+savemat('../datasets/snr_user.mat', {'snr_user': snr, 'M': M, 'K': K, 'N': N, 'pilotMatrix4N': pilotMatrix})
+del snr
---
+++ scripts/user_plots.py
@@ -0,0 +1,40 @@
+from scipy.io import loadmat
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# Load dataset
+snr_data = loadmat("..\\datasets\\snr_user.mat")
+h_data = loadmat("..\\datasets\\h_user.mat")
+
+
+# Unpack constants
+K = h_data['K'][0, 0].astype(int)
+M = h_data['M'][0, 0].astype(int)
+N = h_data['N'][0, 0].astype(int)
+
+
+# Get values
+snr = snr_data['snr_user']
+h_user = h_data['h_user']
+
+
+# Plot
+k = 3
+fig, ax = plt.subplots(5, 10)
+for i in range(5):
+    for j in range(10):
+        ax[i, j].plot(np.abs(h_user[10 * i + j, 3, :]))
+        ax[i, j].get_xaxis().set_visible(False)
+        ax[i, j].set_ylim(-0.1e-5, 1e-5 + 0.1e-5)
+
+plt.subplots_adjust(0.079, 0.11, 0.95, 0.88, 0.455, 0.2)
+
+fig, ax = plt.subplots(5, 10)
+for i in range(5):
+    for j in range(10):
+        ax[i, j].plot(np.abs(snr[10 * i + j, :]))
+        ax[i, j].get_xaxis().set_visible(False)
+        ax[i, j].set_ylim(0.7, 41)
+
+plt.subplots_adjust(0.079, 0.11, 0.95, 0.88, 0.455, 0.2)
